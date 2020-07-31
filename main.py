9e34a4cb198edfd62e5cd9f4818e44ea762cb705
smasui-0302 <68088496+smasui-0302@users.noreply.github.com>
2020-07-31 11:32:45 +0900

    修正

---
+++ main.py
@@ -0,0 +1,8 @@
+from solima import Rectangle
+from solima import Square
+
+r = Rectangle(5, 4)
+print('面積:' + str(r.area()))
+
+s = Square(10)
+print('面積:' + str(s.area()))
--- setup.py
+++ setup.py
@@ -3,23 +3,18 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+install_requires = [
+    # 必要な依存ライブラリがあれば記述
+]
+
+packages = [
+    'solima'
+]
+
 setuptools.setup(
     name="solima",
     version="0.1.0",
-    author="",
-    author_email="",
-    description="sample package'",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/smasui-0302/python-sample-package
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    entry_points = {
-        'console_scripts': ['sample_command = sample_command.sample_command:main']
-    },
-    python_requires='>=3.7',
+    packages=packages,
+    install_requires=install_requires,
+    entry_points={'console_scripts': console_scripts},
 )
